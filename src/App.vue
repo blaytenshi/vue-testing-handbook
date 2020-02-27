e96657855fa8af297b101147aefd472e122fe3c1
Jimmy Tang <blay.tenshi@gmail.com>
2020-02-27 17:43:34 +1100

    Setting up for TDD > Writing the test > Running the test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Greeting/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Greeting from './components/Greeting'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Greeting
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
