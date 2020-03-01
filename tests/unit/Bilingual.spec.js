ec78856b8a00a00ce1eaecede73c40c4933e7251
Jimmy Tang <blay.tenshi@gmail.com>
2020-03-02 02:15:56 +1100

    Mocking global objects > The mocks mounting option

---
+++ tests/unit/Bilingual.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils"
+import Bilingual from "@/components/Bilingual.vue"
+
+describe("Bilingual", () => {
+  it("renders successfully", () => {
+    const wrapper = shallowMount(Bilingual, {
+      mocks: {
+        $t: (msg) => msg
+      }
+    })
+  })
+});
