1e570c9ee0ad6392aa7af3058b8c3c1af2433783
Jimmy Tang <blay.tenshi@gmail.com>
2020-03-02 02:22:06 +1100

    Mocking global objects > Settings default mocks using config

--- tests/unit/Bilingual.spec.js
+++ tests/unit/Bilingual.spec.js
@@ -1,12 +1,18 @@
-import { shallowMount } from "@vue/test-utils"
+import { shallowMount, config } from "@vue/test-utils"
 import Bilingual from "@/components/Bilingual.vue"
+import translations from "../../src/components/translation"
+
+const locale = "ja";
+// const localse = "en";
+
+// here we're using the test utils config object to set a default mock instead of
+// using the mock option in the shallowMount
+config.mocks["$t"] = msg => translations[locale][msg];
 
 describe("Bilingual", () => {
   it("renders successfully", () => {
-    const wrapper = shallowMount(Bilingual, {
-      mocks: {
-        $t: (msg) => msg
-      }
-    })
+    const wrapper = shallowMount(Bilingual);
+
+    console.log(wrapper.html());
   })
 });
