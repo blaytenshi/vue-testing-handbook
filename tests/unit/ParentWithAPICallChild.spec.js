1d832c834256977fa20b354e8d57214ae8a111c2
Jimmy Tang <blay.tenshi@gmail.com>
2020-03-02 08:40:30 +1100

    Mocking global objects > Using stubs to stub <ComponentWithAsyncCall> 1

--- tests/unit/ParentWithAPICallChild.spec.js
+++ tests/unit/ParentWithAPICallChild.spec.js
@@ -8,12 +8,14 @@ describe('ParentWithAPICallChild.vue', () => {
     // you'll see a console log of it
     const wrapper = mount(ParentWithAPICallChild, {
       stubs: {
-        ComponentWithAsyncCall
+        ComponentWithAsyncCall: true // replace all instances of ComponentWithAsyncCall with ComponentWithAsyncCall-stub
       }
     });
 
+    // see the component stubbed out
+    console.log("ParentWithAPICallChild", wrapper.html());
+
     // you can use find() with the component name
     expect(wrapper.find(ComponentWithAsyncCall).exists()).toBe(true);
-    console.log(wrapper.html());
   });
 });
