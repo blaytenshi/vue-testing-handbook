48b8029ebe5c637b6f1d7484b62b1aaa0b9cc6c6
Jimmy Tang <blay.tenshi@gmail.com>
2020-03-02 08:31:35 +1100

    Mocking global objects > The mocks mounting option

--- tests/unit/ParentWithAPICallChild.spec.js
+++ tests/unit/ParentWithAPICallChild.spec.js
@@ -4,8 +4,11 @@ import ComponentWithAsyncCall from "../../src/components/ComponentWithAsyncCall"
 
 describe('ParentWithAPICallChild.vue', () => {
   it('renders with mount and does initialise API call', () => {
+    // fully renders the child components executing the API call
+    // you'll see a console log of it
     const wrapper = mount(ParentWithAPICallChild);
 
+    // you can use find() with the component name
     expect(wrapper.find(ComponentWithAsyncCall).exists()).toBe(true);
-  })
+  });
 });
